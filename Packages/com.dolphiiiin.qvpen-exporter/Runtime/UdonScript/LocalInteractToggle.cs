ce11c7aaeab0209d907fbc89adbc3d46f7f938a6
Naoshige Tozawa <42102311+Dolphiiiin@users.noreply.github.com>
2025-01-17 15:32:33 +0900

    1.2.1 ステージされていなかったファイルをステージ

---
+++ Packages/com.dolphiiiin.qvpen-exporter/Runtime/UdonScript/LocalInteractToggle.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LocalInteractToggle : UdonSharpBehaviour
+{
+    [SerializeField]
+    private GameObject toggleTarget;
+    
+    public override void Interact()
+    {
+        // ターゲットのアクティブ状態を反転
+        toggleTarget.SetActive(!toggleTarget.activeSelf);
+    }
+}
+
